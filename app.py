09c7ddec24c908adacd26eb2628956a5167b7af5
patidarashu <patidarashish388@gmail.com>
2024-05-06 01:39:42 +0530

    Database Created

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+app.secret_key = 'your_secret_key_here'  # Set a secret key for session management
+
+# Sample user data (in a real application, this would be stored in a database)
+users = {
+    'user1': 'password1',
+    'user2': 'password2'
+}
 
 @app.route('/')
 def index():
@@ -16,6 +24,41 @@ def mehndi():
 @app.route('/makeup')
 def makeup():
     return render_template('makeup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         if username in users and users[username] == password:
+#             session['username'] = username  # Store the username in the session
+#             return redirect('/')
+#         else:
+#             return render_template('login.html', error='Invalid username or password')
+#     return render_template('login.html')
+
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         if username not in users:
+#             users[username] = password  # Store the new user in the users dictionary
+#             return redirect('/login')
+#         else:
+#             return render_template('signup.html', error='Username already exists')
+#     return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
